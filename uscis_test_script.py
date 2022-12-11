81570624ccd8ca0d05cb7d92ecd0407e965372f8
Vedanth Narayanan <vedanth.nd@gmail.com>
2022-12-10 18:52:13 -0800

    Form gets submitted successfully

--- uscis_test_script.py
+++ uscis_test_script.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pprint import pprint
 
 print("Begin...")
 
@@ -22,25 +23,33 @@ office = browser.find_element("id", "selectOfficeOrCenter")
 # print(category)
 # print(office)
 
-form_options = [x for x in form.find_elements(By.TAG_NAME, "option")]
-# print(form_options)
+form_options = [x.text for x in form.find_elements(By.TAG_NAME, "option")][1:]
+print(f'There are {len(form_options)} forms to choose from')
 
-for element in form_options:
-    print(element)
+# Creating a dictionary, so I can quickly call what I want - yay
+form_opts_dict = {x.split(' | ')[0]: x for x in form_options}
+pprint(form_opts_dict)
 
-# print("Set fields, delay 3 seconds between input")
-#
-# search_journal = "Relatorios dos Presidentes dos Estados Brasileiros (BA)"
-# search_timeRange = "1890 - 1899"
-# search_text = "Milho"
-#
-# journal.send_keys(search_journal)
+print("Set fields, delay 1 seconds between input")
+
+form_check_key = 'I-129F'
+category_inner_key = 'K1/K2/K3/K4 - Fiance(e) or spouse and/or dependent children'
+service_center_key = 'Potomac Service Center'
+
+form.send_keys(form_opts_dict[form_check_key])
+time.sleep(1)
+category.send_keys(category_inner_key)
+time.sleep(1)
+office.send_keys(service_center_key)
+
+print("Perform search")
+
+submitButton = browser.find_element(By.ID, "getProcTimes")
+submitButton.click()
+
+print("Form has been submitted!")
 # time.sleep(3)
-# timeRange.send_keys(search_timeRange)
-# time.sleep(3)
-# searchTerm.send_keys(search_text)
 #
-# print("Perform search")
-#
-# submitButton = button.find_element_by_id("PesquisarBtn1_input")
-# submitButton.click()
+# result = browser.find_element(By.ID, "range")
+# result_unit = browser.find_element(By.ID, "unit")
+# print(f"This form's estimated processing time today is {result.text} {result_unit.text}.")
