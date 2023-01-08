a3cf26046ea729b3d39cf3ce36c161ea62d59302
Vedanth Narayanan <vedanth.nd@gmail.com>
2023-01-07 22:35:21 -0800

    Tracking requests and responses

--- src/samples/uscis_test_script.py
+++ src/samples/uscis_test_script.py
@@ -1,17 +1,34 @@
 import time
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver import DesiredCapabilities
+# from selenium import webdriver
+
+# from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from pprint import pprint
+from seleniumwire import webdriver
 
+# DesiredCapabilities handlSSLErr = DesiredCapabilities.chrome ()
+# handlSSLErr.setCapability (CapabilityType.ACCEPT_SSL_CERTS, true)
+# WebDriver driver = new ChromeDriver (handlSSLErr);
 
 print("Begin...")
 
-browser = webdriver.Chrome()
 url = "https://egov.uscis.gov/processing-times/"
+# browser = webdriver.Chrome()
+options = {
+    'addr': '127.0.0.1',  # Address of the machine running Selenium Wire. Explicitly use 127.0.0.1 rather than localhost if remote session is running locally.
+    'verify_ssl': False
+}
+browser = webdriver.Chrome(
+    # command_executor=url,
+    seleniumwire_options=options
+)
+
 browser.get(url)
+# browser = driver
 
 print("Waiting to load page... (Delay 3 seconds)")
 
@@ -23,12 +40,12 @@ form = browser.find_element("id", "selectForm")
 category = browser.find_element("id", "selectFormCategory")
 office = browser.find_element("id", "selectOfficeOrCenter")
 
-# form_check_key = 'I-129F'
-# category_inner_key = 'K1/K2/K3/K4 - Fiance(e) or spouse and/or dependent children'
-# service_center_key = 'Potomac Service Center'
+form_check_key = 'I-129F'
+category_inner_key = 'K1/K2/K3/K4 - Fiance(e) or spouse and/or dependent children'
+service_center_key = 'Potomac Service Center'
 
-form_check_key = 'I-765'
-category_inner_key = 'Based on an approved, concurrently filed, I-821D [(c)(33)]'
+# form_check_key = 'I-765'
+# category_inner_key = 'Based on an approved, concurrently filed, I-821D [(c)(33)]'
 
 print("Set fields, delay 1 seconds between input")
 
@@ -68,8 +85,22 @@ for i in office_opts_dict.keys():
     submitButton.click()
     time.sleep(3)
 
+    # pprint(browser.requests)
+    request = browser.requests[-1]
+
+    # for request in browser.requests:
+    if request.response:
+        print(
+            request.url,
+        )
+        # pprint(request.response.body)
+        pprint(request.response.body.decode())
+    time.sleep(1)
+
     result = browser.find_element(By.CLASS_NAME, "range").text.replace('\n', ' ')
     print(f'result - {result}')
+
+    break
     # # Final class text has a newline character, and we're removing it here
     # final = result.text.replace('\n', ' ')
     # print(f"This form's estimated processing time today at {office_opts_dict[i]} is {final}.")
